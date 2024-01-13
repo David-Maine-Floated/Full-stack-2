2f3829e0c0985aa7933541b1e42a8742250e8b97
David <dmaine6711@gmail.com>
2024-01-13 13:12:43 -0500

    connect sign up modal to "start reading button"
    "

--- frontend/components/splash/UnLoggedInSplash.jsx
+++ frontend/components/splash/UnLoggedInSplash.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
 import './UnLoggedInSplash.css'
+import { useDispatch } from "react-redux";
+import { showModal } from "../../src/store/modals";
 const UnLoggedInSplash = () => {
-
+  const dispatch = useDispatch()
 
     return (
       <div className="splash">
-        <h2 >Stay curious.</h2>
+        <h2>Stay curious.</h2>
         <h3>
-          Discover stoires, thinking, and <br></br> expertiese from writers on any topic.
+          Discover stoires, thinking, and <br></br> expertiese from writers on
+          any topic.
         </h3>
         <div className="startReadingButton">
-          <Link className="startReadingLink" to="signup">
+          <Link
+            className="startReadingLink"
+            to="/"
+            onClick={() => dispatch(showModal("signup"))}
+          >
             Start Reading
           </Link>
         </div>
