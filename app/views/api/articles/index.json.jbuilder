7e82987582e67188100b69b5d753cbf1c937e197
David <dmaine6711@gmail.com>
2024-01-21 15:14:21 -0500

    add images and date to article index

--- app/views/api/articles/index.json.jbuilder
+++ app/views/api/articles/index.json.jbuilder
@@ -2,5 +2,6 @@
 
     json.array! @articles do |article|
     json.extract! article,  :title, :body, :topics, :created_at, :updated_at, :author_id, :id
+    json.photoUrl article.photo.attached? ? article.photo.url : nil
     end
 
--- frontend/components/articleIndex/indexItem.jsx
+++ frontend/components/articleIndex/indexItem.jsx
@@ -9,34 +9,58 @@ const IndexItem = ({article, author}) => {
     e.preventDefault();
     navigate(`article/${article.id}`)
   }
+
+
+  const originalDate = new Date(article.createdAt);
+
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+    originalDate
+  );
   
+  console.log('ARTICLE', article)
+
     return (
-      <div className="indexItemContainer">
-        <div className="userBarIndex">
-          <div className="userPhotoContainer">
-            <img className="userPhoto" src={author && author.photoUrl} alt="" />
+      <div className="indexItem">
+        <div className="indexItemContainer">
+          <div className="userBarIndex">
+            <div className="userPhotoContainer">
+              <img
+                className="userPhoto"
+                src={author && author.photoUrl}
+                alt=""
+              />
+            </div>
+            <div className="indexItemAuthorNameDiv">
+              <span className="indexItemAuthorName">
+                {author && author.username}
+              </span>
+            </div>
+            <div className="createdAt">
+              <span>{".  " + formattedDate}</span>
+            </div>
           </div>
-          <div className="indexItemAuthorNameDiv">
-            <span className="indexItemAuthorName">
-              {author && author.username}
-            </span>
+          <div className="indexItemTitleDiv" onClick={handleNavClick}>
+            <h2 className="indexItemTitle">{article.title}</h2>
+          </div>
+          <div className="indexItemBodyDiv" onClick={handleNavClick}>
+            <p className="indexItemBody">
+              {article.body.slice(0, 150) + "..."}
+            </p>
+          </div>
+          <div className="indexItemFooter">
+            <div className="indexItemFooterLeftDiv">
+              <span className="indexItemFooterSpan">Topic</span>
+              <span className="indexItemFooterSpan">8 Minute Read</span>
+            </div>
+            <div className="indexItemFooterRightDiv">
+              <span className="indexItemFooterSpan">Save Icon</span>
+            </div>
           </div>
         </div>
-        <div className="indexItemTitleDiv" onClick={handleNavClick}>
-          <h2 className="indexItemTitle">{article.title}</h2>
-        </div>
-        <div className="indexItemBodyDiv" onClick={handleNavClick}>
-          <p className="indexItemBody">{article.body.slice(0, 150) + "..."}</p>
-        </div>
-        <div className="indexItemFooter">
-          <div className="indexItemFooterLeftDiv">
-            <span className="indexItemFooterSpan">Topic</span>
-            <span className="indexItemFooterSpan">8 Minute Read</span>
+          <div className="index imageContainer">
+            <img onClick={handleNavClick} className="indexImage" src={article.photoUrl} alt="" />
           </div>
-          <div className="indexItemFooterRightDiv">
-            <span className="indexItemFooterSpan">Save Icon</span>
-          </div>
-        </div>
       </div>
     );
 }
