65fa9c67b371a4125fc4ac3d13d90e3b31bbc762
David <dmaine6711@gmail.com>
2024-01-25 14:00:16 -0500

    fix commen model syntax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,13 @@ class Comment < ApplicationRecord
     validates :body, :commenter_id, :article_id, presence: true 
     validates :body, inclusion: {in: 1..500}
 
-    belongs_to: :article 
+    belongs_to :article 
 
-    has_many: :replies 
+    has_many :replies, 
         foreign_key: :parent_comment_id,
         class_name: :Comment 
 
-    belongs_to: :parent_comment, 
+    belongs_to :parent_comment, 
         foreign_key: :parent_comment_id,
         class_name: :Comment,
         optional: true
