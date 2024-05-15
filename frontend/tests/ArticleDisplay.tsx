b0ba8724b91d4f69ae017a857f710b184b751a46
David <dmaine6711@gmail.com>
2024-05-15 11:24:25 -0400

    configure react testing library, jsdom, vitest

---
+++ frontend/tests/ArticleDisplay.tsx
@@ -0,0 +1,9 @@
+import { it, expect, describe } from 'vitest'
+import {render} from '@testing-library/react'
+import ArticleDisplay from '../components/articles/ArticleDisplay'
+
+describe('ArticleDislpay', () => {
+    it('should', () => {
+        render(<ArticleDisplay/>)
+    })
+})
---
+++ frontend/tests/main.test.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe } from 'vitest'
+
+describe('Test', () => {
+    it('testtest', () => {
+        expect(1).toBeTruthy()
+    })
+})
+
